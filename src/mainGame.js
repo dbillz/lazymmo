91dc61b5c0fbdc6f11f694d7f7cb2f4fdf1dd113
dbillz1 <safwem@gmail.com>
2015-07-17 20:36:04 +0000

    Implemented basic item/inventory/equipped item system

--- src/mainGame.js
+++ src/mainGame.js
@@ -1,6 +1,26 @@
 var mainGame = function(game){
     this.textShown = "hola mundo ";
     this.textStyle = { font: '34px Arial', fill: '#f0f' };
+    
+    
+    this.HEAD_SLOT = 'head';
+    this.LEG_SLOT = 'leg';
+    this.CHEST_SLOT = 'chest';
+    this.FOOT_SLOT = 'foot';
+    this.WEAPON_SLOT = 'weapon';
+    this.BACK_SLOT = 'back';
+    this.BELT_SLOT = 'belt';
+    this.NECK_SLOT = 'neck';
+    this.RING_SLOT = 'ring';
+    this.HAND_SLOT = 'hand';
+    this.SHOULDER_SLOT = 'shoulder';
+    
+    this.COMMON_RARITY = 'common';
+    this.UNCOMMON_RARITY = 'uncommon';
+    this.RARE_RARITY = 'rare';
+    this.EPIC_RARITY = 'epic';
+    
+    
 }
 
 mainGame.prototype = {
@@ -10,10 +30,59 @@ mainGame.prototype = {
     },
     
     create:function(){
-        this.game.add.text(10,10,this.textShown, this.textStyle)
+        this.game.add.text(10,10,this.textShown, this.textStyle);
+        this.player = this.createPlayer();
     },
     
     update: function(){
+        
+    },
+    
+    createPlayer: function(){
+        var player = {};
+        player.coins = 0;
+        player.equipment = [];
+        //Players start with chest, legs, and weapon.
+        //No gloves, necklace, shoulder,  head given at start.
+        
+        player.equipment[this.CHEST_SLOT] = this.createItem(this.CHEST_SLOT,0);
+        player.equipment[this.LEG_SLOT] = this.createItem(this.LEG_SLOT,0);
+        player.equipment[this.WEAPON_SLOT] = this.createItem(this.WEAPON_SLOT,0);
+        
+        player.inventory = [];
+        return player;
+    },
+    
+    equipItem: function(item){
+        
+        //If there's already something in that slot,
+        if(typeof this.player.equipment[item.slot] != 'undefined' && this.player.equipment[item.slot] != null){
+            this.unequipItem(item.slot);
+        } 
+        //Then place the item in the newly empty slot
+        this.player.equipment[item.slot] = item;
+        
+    },
+    
+    unequipItem: function(slot){
+        if(typeof this.player.equipment[slot] != 'undefined' && this.player.equipment[slot] != null){
+            this.player.inventory.push(this.player.equipment[slot]);
+        } 
+    },
+    
+    createItem: function(slot, rarity){
+        var item = {};
+        item.slot = slot;
+        switch(rarity){
+            case 0: item.rarity = this.COMMON_RARITY; break;
+            case 1: item.rarity = this.UNCOMMON_RARITY; break;
+            case 2: item.rarity = this.RARE_RARITY; break;
+            case 3: item.rarity = this.EPIC_RARITY; break;
+        }
+        return item;
+    },
+    
+    getSpriteForItem: function(item){
         
     }
     
